73d8bf1d9361558389deff2e45718cd85e443df0
carly-eisen <carlyagerber@gmail.com>
2021-02-07 23:57:00 -0600

    commit tree 1e1a67cdfaeafa3fefba67a139b92c3463dca276

---
+++ lib/song.rb
@@ -0,0 +1,29 @@
+class Song
+
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+   def initialize(song_name, artist, genre)
+        @name = song_name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    # def self.count
+    # end    
+
+end
